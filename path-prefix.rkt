eb75d564f61a362c5853cfc3bf0de4d5ea9b5fef
srfoster <stephen@thoughtstem.com>
2019-11-12 11:03:56 -0800

    path prefix utility and some bootstrap stuff

--- path-prefix.rkt
+++ path-prefix.rkt
@@ -3,25 +3,40 @@
 (provide path-prefix
          with-prefix
          add-path-prefix
-         pathify)
+         pathify
+         prefix/pathify)
 
 (define path-prefix (make-parameter #f))
 
+(define (simplify-slashes s)
+  (regexp-replaces s
+                   `([#rx"//" "/"])))
+
+(define (prefix/pathify p)
+  (pathify
+    (add-path-prefix p)))
+
+;Crunches lists down to strings.
 (define (pathify path)
-  (cond
+  (simplify-slashes
+    (cond
       [(string? path) 
        (if (string-prefix? path "/")
          path
          (~a "/" path))]
-      [(list? path) (~a "/" (string-join path "/"))]))
+      [(list? path) (~a "/" (string-join path "/"))])))
 
+;Keeps paths as lists (if they are), but pushes the prefix
 (define (add-path-prefix path)
   (if (not (path-prefix))
     path
     (cond
       [(string? path) (~a "/" (path-prefix) "/" path)]
-      [(list? path) (cons (path-prefix) path)])))
+      [(list? path) (cons (path-prefix) path)])) )
 
 (define-syntax-rule (with-prefix prefix do-stuff ...)
   (parameterize ([path-prefix prefix])
     do-stuff ...))
+
+
+
