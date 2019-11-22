92ace45050098f78848d653c1e9b03ae0d728d21
aBlender <JasonLongLe@gmail.com>
2019-11-21 19:28:27 -0800

    set 2 column row for both sm and md

--- bootstrap.rkt
+++ bootstrap.rkt
@@ -118,7 +118,7 @@
 (define (responsive-row #:columns columns . items)
   (define row-size (max 1 (min 12 (exact-round (/ 12 columns)))))
   (apply row (map (curry div class: (~a "col-lg-" row-size
-                                        " col-xs-12 my-3")) items)))
+                                        " col-sm-6 col-xs-12 my-3")) items)))
 
 
 
