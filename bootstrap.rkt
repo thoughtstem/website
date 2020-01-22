5359a96a5f290f2288cccf81a72c6d54ada9eddf
Stephen Foster <stephen@thoughtstem.com>
2020-01-22 13:51:00 -0800

    making defered css loading an option, not the default

--- bootstrap.rkt
+++ bootstrap.rkt
@@ -74,12 +74,13 @@
     (include-js "/js/p5.min.js" #:tag 'async)))
 
 (define (content #:head (head-content '()) 
+		 #:defer-css (defer #f)
                  .  body-content)
   (html
     (head
       ;head-content
-      (include-bootstrap-css #:defer #t)
-      (include-font-awesome-css #:defer #t)
+      (include-bootstrap-css #:defer defer)
+      (include-font-awesome-css #:defer defer)
       (meta 'charset: "utf-8")
       (meta name: "viewport"
             content: "width=device-width, initial-scale=1, shrink-to-fit=no")
