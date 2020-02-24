25430a5afbc465173962a0d57a6af58a4ee3af0e
aBlender <JasonLongLe@gmail.com>
2020-02-24 14:18:31 -0800

    Adding margins to badge pills

--- bootstrap/badges.rkt
+++ bootstrap/badges.rkt
@@ -5,84 +5,84 @@
 (define/provide-extensible-element 
   badge-primary
   span
-  (class: "badge badge-primary" class-join))
+  (class: "m-1 badge badge-primary" class-join))
 
 (define/provide-extensible-element 
   badge-secondary
   span
-  (class: "badge badge-secondary" class-join))
+  (class: "m-1 badge badge-secondary" class-join))
 
 (define/provide-extensible-element 
   badge-success
   span
-  (class: "badge badge-success" class-join))
+  (class: "m-1 badge badge-success" class-join))
 
 (define/provide-extensible-element 
   badge-danger
   span
-  (class: "badge badge-danger" class-join))
+  (class: "m-1 badge badge-danger" class-join))
 
 (define/provide-extensible-element 
   badge-warning
   span
-  (class: "badge badge-warning" class-join))
+  (class: "m-1 badge badge-warning" class-join))
 
 (define/provide-extensible-element 
   badge-info
   span
-  (class: "badge badge-info" class-join))
+  (class: "m-1 badge badge-info" class-join))
 
 (define/provide-extensible-element 
   badge-light
   span
-  (class: "badge badge-light" class-join))
+  (class: "m-1 badge badge-light" class-join))
 
 (define/provide-extensible-element 
   badge-dark
   span
-  (class: "badge badge-dark" class-join))
+  (class: "m-1 badge badge-dark" class-join))
 
 
 
 (define/provide-extensible-element 
   badge-pill-primary
   span
-  (class: "badge badge-pill badge-primary" class-join))
+  (class: "m-1 badge badge-pill badge-primary" class-join))
 
 (define/provide-extensible-element 
   badge-pill-secondary
   span
-  (class: "badge badge-pill badge-secondary" class-join))
+  (class: "m-1 badge badge-pill badge-secondary" class-join))
 
 (define/provide-extensible-element 
   badge-pill-success
   span
-  (class: "badge badge-pill badge-success" class-join))
+  (class: "m-1 badge badge-pill badge-success" class-join))
 
 (define/provide-extensible-element 
   badge-pill-danger
   span
-  (class: "badge badge-pill badge-danger" class-join))
+  (class: "m-1 badge badge-pill badge-danger" class-join))
 
 (define/provide-extensible-element 
   badge-pill-warning
   span
-  (class: "badge badge-pill badge-warning" class-join))
+  (class: "m-1 badge badge-pill badge-warning" class-join))
 
 (define/provide-extensible-element 
   badge-pill-info
   span
-  (class: "badge badge-pill badge-info" class-join))
+  (class: "m-1 badge badge-pill badge-info" class-join))
 
 (define/provide-extensible-element 
   badge-pill-light
   span
-  (class: "badge badge-pill badge-light" class-join))
+  (class: "m-1 badge badge-pill badge-light" class-join))
 
 (define/provide-extensible-element 
   badge-pill-dark
   span
-  (class: "badge badge-pill badge-dark" class-join))
+  (class: "m-1 badge badge-pill badge-dark" class-join))
 
 
 
