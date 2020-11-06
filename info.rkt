cb8f485b446ab358e254c29bbbf8d2244b12fa29
Stephen Foster <stephen@thoughtstem.com>
2020-11-06 10:58:51 -0800

    new raco command

--- info.rkt
+++ info.rkt
@@ -8,4 +8,5 @@
 (define pkg-authors '(thoughtstem))
 (define raco-commands
   '(("website-preview" (submod website/raco-tools/preview main) "runs a server in your current directory and serves everything" 100)
+    ("website-s3-deploy" (submod website/raco-tools/s3-deploy main) "Deploys to S3" 100)
     ))
